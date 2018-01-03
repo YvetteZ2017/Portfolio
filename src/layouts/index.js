a91e50ce72a15d21434fc63ee8bc3c50043c6177
Yvette Zhang <yvettez2016@gmail.com>
2018-01-03 15:07:00 -0500

    debu: refactored layouts

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,25 @@
 import React from "react";
+import Link from "gatsby-link";
+
+const ListLink = props =>
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>
+      {props.children}
+    </Link>
+  </li>
 
 export default ({ children }) => (
-  <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
-    {children}
+  <div style={{ margin: `0 auto`, maxWidth: '50vw', padding: `1.25rem 1rem` }}>
+  <header style={{ marginBottom: `1.5rem` }}>
+    <Link to="/" style={{ textShadow: `none`, backgroundImage: `none`, color: 'tomato' }}>
+      <h3 style={{ display: `inline` }}>MySweetSite</h3>
+    </Link>
+    <ul style={{ listStyle: `none`, float: `right` }}>
+      <ListLink to="/">Home</ListLink>
+      <ListLink to="/about/">About</ListLink>
+      <ListLink to="/contact/">Contact</ListLink>
+    </ul>
+  </header>
+    {children()}
   </div>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,12 @@ import Main from './Main';
 import BasicLayout from '../layouts';
 
 export default () =>
-  <div style={{ color: `yellow` }}>
+  <div style={{ color: `tomato` }}>
     <h1>Hello Gatsby!</h1>
     <p>What a world.</p>
     <img src="/static/photos/moon.jpg" alt="" />
     <br />
-    <div>
-				<Link to="/about/">About</Link>
-				<Link to="/contact/">contact</Link>
-    </div>
+    
     <br />
     <Main />
 	</div>
