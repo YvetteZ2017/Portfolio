fa0353fc95726477530be49086da79c6ca2a93f6
Yvette Zhang <yvettez2016@gmail.com>
2018-01-08 16:01:35 -0500

    feat: web icon animation

---
+++ src/components/ProjectItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+export default class ProjectItem extends Component {
+    
+    render() {
+        return (
+            <div>Good Night</div>
+        )
+    }
+}
+
+
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,9 +3,34 @@ import Link from "gatsby-link";
 import g from "glamorous";
 import { css } from "glamor";
 import { rhythm } from "../utils/typography";
+import styles from "./icon-css-style.module.css";
 
-const linkStyle = css({ float: `right`, margin: '1em', textDecoration:'none', color:'black'});
-const contentStyle = css({ margin: `0 auto`, maxWidth: '50vw', padding: 'rhythm(2)', paddingTop: 'rhythm(1.5)'})
+
+const linkStyle = css({
+  float: `right`,
+  margin: '1em',
+  textDecoration:'none',
+  color:'black'
+});
+
+const contentStyle = css({
+  margin: `0 auto`,
+  maxWidth: '50vw',
+  padding: 'rhythm(2)',
+  paddingTop: 'rhythm(1.5)'
+});
+
+const iconStyle = css({
+  marginBottom: 'rhythm(2)',
+  display: `inline-block`,
+  fontStyle: `normal`,
+  fontWeight: 200,
+  textDecoration: 'underline',
+  fontFamily: 'Times New Roman',
+  color: 'black',
+  letterSpacing: '0.3em',
+  transition: 'all 500 ease-in'
+});
 
 export default ({ children, data }) => 
   <g.Div
@@ -14,20 +39,11 @@ export default ({ children, data }) =>
     padding={rhythm(1.5)}
     paddingTop={rhythm(1.5)}
   >
-    <Link to={`/`}>
-      <g.H3
-        marginBottom={rhythm(2)}
-        display={`inline-block`}
-        fontStyle={`normal`}
-        fontWeight={200}
-        textDecoration={'underline'}
-        fontFamily={'Times New Roman'}
-        color={'black'}
-        letterSpacing={'0.3em'}
-      >
-        {data.site.siteMetadata.title}
-      </g.H3>
-    </Link>
+  <Link to={`/`}>
+    <g.H3 className={styles.icon}>
+        YVETTE ZHANG
+    </g.H3>
+  </Link>
     <Link className={linkStyle} to={`/about/`}>
       About
     </Link>
