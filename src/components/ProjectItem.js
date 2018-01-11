4f85229052bed2fc7261d1e78b7523de8d853d96
Yvette Zhang <yvettez2016@gmail.com>
2018-01-11 16:30:25 -0500

    feat: video in modal

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 import styles from "./project-item.module.css";
 import Modal from 'react-modal';
+import { DefaultPlayer as Video } from 'react-html5video';
+import 'react-html5video/dist/styles.css';
 
 
 export default class ProjectItem extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            openStatus: false
+            openStatus: false,
         }
         this.open = this.open.bind(this);
         this.close = this.close.bind(this);
+        this.embedVideo = this.embedVideo.bind(this);
+        this.linkToVideo = this.linkToVideo.bind(this);
     }
 
     open() {
@@ -25,18 +29,41 @@ export default class ProjectItem extends Component {
         })
     }
 
+    embedVideo(id) {
+        return 'https://www.youtube.com/embed/' + id + '?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&mute=1';
+    }
+
+    linkToVideo(id) {
+        return 'https://www.youtube.com/watch?v=' + id;
+    }
+
     render() {
-        const { imageSrc, title } = this.props;
+        const { imageSrc, title, description, videoId, link, role } = this.props.info;
     
         return (
             <div className={styles.container} onClick={this.open}>
-            <img src={imageSrc} className={styles.image} />
-            <div className={styles.overlay}>
-                <div className={styles.text}>{title}</div>
-            </div>
-            <Modal isOpen={this.state.openStatus} style={modalStyle} shouldCloseOnOverlayClick={true} onRequestClose={this.close}>
-                <iframe src="https://vimeo.com/202093812"></iframe>
-            </Modal>    
+                <img src={imageSrc} className={styles.image} />
+                <div className={styles.overlay}>
+                    <div className={styles.text}>{title}</div>
+                </div>
+                <Modal isOpen={this.state.openStatus} style={modalStyle} shouldCloseOnOverlayClick={true} onRequestClose={this.close}>
+                    
+                    <div className={styles.videoBackground}>
+                        <div className={styles.videoForeground}>
+                            <iframe src={this.embedVideo(videoId)} frameBorder="0" allowfullscreen className={styles.iframe}></iframe>
+                        </div>
+                    </div>
+                    
+                    <div>
+                        <div className={styles.vidInfo}>
+                            <h1>{title}</h1>
+                            <p>{description}</p>
+                            <p>{'My role: ' + role}</p>
+                            {link.length ? <a href={link}>Check out this project</a> : null}
+                            <a href={this.linkToVideo(videoId)}>Full Video</a>
+                        </div>
+                    </div>
+                </Modal>    
             </div>
         );
     }
@@ -45,7 +72,7 @@ export default class ProjectItem extends Component {
 
 const modalStyle = {
     overlay: {
-        backgroundColor: 'rgba(255, 255, 255, 0.75)',
+        backgroundColor: 'rgba(0, 0, 0, 0.7)',
         zIndex: 3,
         position: 'fixed',
         left: 0,
@@ -53,19 +80,17 @@ const modalStyle = {
         width: '100%',
     },
     content: {
-        backgroundColor: '#fff',
-        color: 'white',
-        borderRadius: '4px',
+        color: 'black',
         backgroundRepeat: 'no-repeat',
         outline: 'none',
-        height: '80%',
-        width: '80%',
-        padding: '20px',
+        height: '79.6%',
+        width: '79%',
         margin: 'auto',
         zIndex: 3,
         position: 'fixed',
         justifyContent: 'center',
-        left: 0
+        left: 0,
+        border: 0,
     }
 }
 
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,10 +7,12 @@ export default () => (
     <div className={styles.projectPage}>
         <h1 className={styles.projectPageTitle}>My Projects</h1>
         <div className={styles.projectContainer}>
-            <ProjectItem imageSrc='/static/photos/projects/puyo-game2.png' title='Puyo Stone'/>
-            <ProjectItem imageSrc='/static/photos/projects/puyo-game2.png' title='White Out'/>
-            <ProjectItem imageSrc='/static/photos/projects/puyo-game2.png' title='Grace Shopper'/>
-            <ProjectItem imageSrc='/static/photos/projects/puyo-game2.png' title='Neko School'/>
+        {
+            projectInfo.map((project, i) => (
+                <ProjectItem key={i} info={projectInfo[i]}/>
+            )
+            )
+        }
         </div>
     </div>
 )
@@ -18,30 +20,34 @@ export default () => (
 const projectInfo = [
     {
         title: 'White Out',
-        imageSrc: '/static/photos/projects/puyo-game2.png',
-        videoSrc: '',
-        discription: '',
-        link: ''
+        imageSrc: '/static/photos/projects/weather.jpg',
+        videoId: '',
+        description: 'White Out is a mobile weather app that shows the weather of both the user’s location and other places around the world. The app was build with React-Native and D3. Users are able to search weather of any city via location auto-complete by Google API, switch between displaying temperatures in either Celsius or Fahrenheit units.',
+        role: 'Sole developer over a period of four days for a hackathon competition.',
+        link: 'https://github.com/YvetteZ2017/stackathon',
     },
     {
         title: 'Puyo Stone',
-        imageSrc: '/static/photos/projects/puyo-game2.png',
-        videoSrc: '',
-        discription: '',
+        imageSrc: '/static/photos/projects/puyo.png',
+        videoId: '',
+        description: 'Puyo Stone is a recreation of the Japanese Sega classic game Puyo-Puyo in Javascript using React.js, D3, and Firebase.',
+        role: 'This game was built by a team of four. I was Responsible for D3 SVG animation, rendering and designing game interface.',
         link: 'https://puyost.one',
     },
     {
         title: 'Adopt a Rock',
-        imageSrc: '/static/photos/projects/puyo-game2.png',
-        videoSrc: '',
-        discription: '',
+        imageSrc: '/static/photos/projects/rock.png',
+        videoId: '',
+        description: 'An e-commerce site built with Express and Sequelize on the back end and React and React-Redux on the front end. Users can sign in with their Google account using OAuth and shop safely. It also allows users to browse, search by category, give ratings and leave feedback, purchase items via cart and checkout, and return items. Administrative users can also add, edit, or alter any inventory, descriptions, and pictures.',
+        role: 'This site was built over the course of a week by a team of five.',
         link: 'https://adopt-a-rock.herokuapp.com',
     },
     {
-        title: 'Puyo Stone',
-        imageSrc: '/static/photos/projects/puyo-game2.png',
-        videoSrc: '',
-        discription: '',
-        link: 'https://puyost.one',
+        title: 'Neko School',
+        imageSrc: '/static/photos/projects/neko.png',
+        videoId: 'Muo9MvteI80',
+        description: 'This web app is a coursework assignment based on sequelize, express, react, redux and Bootstrap. It is a RESTful web platform that allows users to manage the students and campuses of a school.',
+        role: 'Sole developer',
+        link: '',
     },
 ]
