0a487219235d4f95733c0ae4d43e3de5f0ab0cd1
Yvette Zhang <yvettez2016@gmail.com>
2018-01-11 11:34:11 -0500

    feat: modal in project items

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,20 +1,72 @@
 import React, { Component } from "react";
 import styles from "./project-item.module.css";
+import Modal from 'react-modal';
 
 
-const ProjectItem = (props) => {
-    const { imageSrc, title } = props
+export default class ProjectItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            openStatus: false
+        }
+        this.open = this.open.bind(this);
+        this.close = this.close.bind(this);
+    }
+
+    open() {
+        this.setState({
+            openStatus: true
+        })
+    }
+
+    close() {
+        this.setState({
+            openStatus: false
+        })
+    }
+
+    render() {
+        const { imageSrc, title } = this.props;
     
-    return (
-        <div className={styles.container}>
-        <img src={imageSrc} className={styles.image} />
-        <div className={styles.overlay}>
-            <div className={styles.text}>{title}</div>
-        </div>
-        </div>
-    )
+        return (
+            <div className={styles.container} onClick={this.open}>
+            <img src={imageSrc} className={styles.image} />
+            <div className={styles.overlay}>
+                <div className={styles.text}>{title}</div>
+            </div>
+            <Modal isOpen={this.state.openStatus} style={modalStyle} shouldCloseOnOverlayClick={true} onRequestClose={this.close}>
+                <iframe src="https://vimeo.com/202093812"></iframe>
+            </Modal>    
+            </div>
+        );
+    }
 }
 
-export default ProjectItem;
 
+const modalStyle = {
+    overlay: {
+        backgroundColor: 'rgba(255, 255, 255, 0.75)',
+        zIndex: 3,
+        position: 'fixed',
+        left: 0,
+        height: '100%',
+        width: '100%',
+    },
+    content: {
+        backgroundColor: '#fff',
+        color: 'white',
+        borderRadius: '4px',
+        backgroundRepeat: 'no-repeat',
+        outline: 'none',
+        height: '80%',
+        width: '80%',
+        padding: '20px',
+        margin: 'auto',
+        zIndex: 3,
+        position: 'fixed',
+        justifyContent: 'center',
+        left: 0
+    }
+}
 
+Modal.setAppElement('#___gatsby')
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,14 +4,44 @@ import styles from './page.module.css';
 import ProjectItem from '../components/ProjectItem';
 
 export default () => (
-    <div className={styles.page}>
-        <h1>My Projects</h1>
+    <div className={styles.projectPage}>
+        <h1 className={styles.projectPageTitle}>My Projects</h1>
         <div className={styles.projectContainer}>
-        <ProjectItem imageSrc='/static/photos/projects/puyo-game2.png' title='Puyo Stone'/>
-        <ProjectItem imageSrc='/static/photos/projects/puyo-game2.png' title='White Out'/>
-        <ProjectItem imageSrc='/static/photos/projects/puyo-game2.png' title='Grace Shopper'/>
-        <ProjectItem imageSrc='/static/photos/projects/puyo-game2.png' title='Neko School'/>
-        
+            <ProjectItem imageSrc='/static/photos/projects/puyo-game2.png' title='Puyo Stone'/>
+            <ProjectItem imageSrc='/static/photos/projects/puyo-game2.png' title='White Out'/>
+            <ProjectItem imageSrc='/static/photos/projects/puyo-game2.png' title='Grace Shopper'/>
+            <ProjectItem imageSrc='/static/photos/projects/puyo-game2.png' title='Neko School'/>
         </div>
     </div>
 )
+
+const projectInfo = [
+    {
+        title: 'White Out',
+        imageSrc: '/static/photos/projects/puyo-game2.png',
+        videoSrc: '',
+        discription: '',
+        link: ''
+    },
+    {
+        title: 'Puyo Stone',
+        imageSrc: '/static/photos/projects/puyo-game2.png',
+        videoSrc: '',
+        discription: '',
+        link: 'https://puyost.one',
+    },
+    {
+        title: 'Adopt a Rock',
+        imageSrc: '/static/photos/projects/puyo-game2.png',
+        videoSrc: '',
+        discription: '',
+        link: 'https://adopt-a-rock.herokuapp.com',
+    },
+    {
+        title: 'Puyo Stone',
+        imageSrc: '/static/photos/projects/puyo-game2.png',
+        videoSrc: '',
+        discription: '',
+        link: 'https://puyost.one',
+    },
+]
