12784e23cb0677388ba0401aa81da3a3bcd030c5
Yvette Zhang <yvettez2016@gmail.com>
2018-02-06 13:49:34 -0500

    revi: revised layout on blog-post pages

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,14 +1,20 @@
 import React from "react";
+import Footer from '../components/Footer';
+import Header from '../components/Header';
 import styles from './blog-post.module.css';
 
 export default ({ data }) => {
   const post = data.markdownRemark;
 
   return (
-    <div className={styles.container}>
-      <h1>{post.frontmatter.title}</h1>
-      <h6 className={styles.date}>{post.frontmatter.date}</h6>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
+    <div className={styles.flex}>
+      <Header color='black' />
+      <div className={styles.container}>
+        <h1>{post.frontmatter.title}</h1>
+        <h6 className={styles.date}>{post.frontmatter.date}</h6>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+      <Footer color='black' />
     </div>
   );
 };
